3993d13269731ee93e46fbbf8af7c799c50cc98b
Shafournee <31718336+Shafournee@users.noreply.github.com>
2019-02-18 20:53:44 -0500

    added stuff

---
+++ Assets/Scripts/CircleDropper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleDropper : MonoBehaviour
+{
+    [SerializeField] GameObject layoutGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void colorize(GameObject slot)
+    {
+        Color color = Color.white;
+        slot.GetComponent<Image>().color = color;
+    }
+}
